ca28cff41f3c90a8fa9aee13d764ca9e32c0a6fa
istota rakotwórcza <fistaszek500@gmail.com>
2026-08-21 09:25:25 +0200

    added red theme

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 let message = document.getElementById("copyMsg");
+const video = document.getElementById('bgVid');
+const source = document.getElementById('vidSource');
 
 function copydiscord()
 {
     navigator.clipboard.writeText("peaqnut");
     
     message.innerHTML = "Copied to Clipboard";
-    setTimeout(function() { message.innerHTML = ""; }, 1500) // theres probably a much better way i just dont know how
+    setTimeout(function() { message.innerHTML = ""; }, 1500) // theres probably a much better way
+}
+
+function theme(str)
+{
+    switch(str)
+    {
+        case "blue":
+        {
+            source.src = "assets/0000-0288.mp4";
+            video.load();
+            document.documentElement.style.setProperty("--bgColor", "rgb(70, 77, 110)");
+            document.documentElement.style.setProperty("--Color", "rgb(106, 113, 150)");
+            document.documentElement.style.setProperty("--hoverColor", "rgb(85, 91, 129)");
+            document.documentElement.style.setProperty("--strokeColor", "rgb(133, 137, 160)");
+            document.documentElement.style.setProperty("--textColor", "#bfc4eb");
+            break;
+        }
+        case "red":
+        {
+            source.src = "assets/0001-0250.mp4";
+            video.load();
+            document.documentElement.style.setProperty("--bgColor", "rgb(110, 70, 70)");
+            document.documentElement.style.setProperty("--Color", "rgb(150, 106, 106)");
+            document.documentElement.style.setProperty("--hoverColor", "rgb(129, 85, 85)");
+            document.documentElement.style.setProperty("--strokeColor", "rgb(160, 133, 133)");
+            document.documentElement.style.setProperty("--textColor", "#ebbfbf");
+            break;
+        }
+    }
 }
